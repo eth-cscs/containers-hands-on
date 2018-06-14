f47b00d51f2401552b0acffa96ee84c9679b45c1
Mark Klein <klein@cscs.ch>
2018-06-14 14:03:02 +0200

    removed unneeded command

--- jupyter.sh
+++ jupyter.sh
@@ -8,9 +8,8 @@ USERNAME=<username>
 # Recomended 32768-61000
 PORT=432<usernum>
 
-# Keep this 1
 NUMNODES=1
 
 
-ssh -t -L${PORT}:127.0.0.1:${PORT} -4 -l${USERNAME} ela.cscs.ch /users/kleinm/jupyterkey\;ssh-add \~/jupyter\;ssh -t -4 -L${PORT}:127.0.0.1:${PORT} daint module load analytics\\\;salloc --res=hpccrs02 -N${NUMNODES} -Cgpu start_analytics --login-port ${PORT} --ui-port ${PORT} --run-cmd \\\"jupyter notebook --port ${PORT}\\\"
+ssh -t -L${PORT}:127.0.0.1:${PORT} -4 -l${USERNAME} ela.cscs.ch /users/kleinm/jupyterkey\;ssh -t -4 -L${PORT}:127.0.0.1:${PORT} daint module load analytics\\\;salloc --res=hpccrs02 -N${NUMNODES} -Cgpu start_analytics --login-port ${PORT} --ui-port ${PORT} --run-cmd \\\"jupyter notebook --port ${PORT}\\\"
 
